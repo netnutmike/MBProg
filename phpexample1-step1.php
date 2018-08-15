e9ecdf36f86faf30bc55f93c09877ff852a8aebb
netnutmike <mike@geniusideastudio.com>
2018-08-14 23:51:59 -0400

    Add files via upload

---
+++ phpexample1-step1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Learning PHP With the Boy Scouts</title>
+</head>
+<body>
+<h1>Programming Merit Badge</h1>
+    
+Hi, I am  and I am learning php for my programming Merit Badge.<br><br>
+    
+Today is <br><br>
+    
+I was born on <br><br>
+    
+<a href="/phpexample1.php">Reset by clicking here</a>
+    
+    <form>
+    Your Name: <input type="text" name="myname"><br>
+    Birthdate in yyyy-mm-dd: <input type="text" name="birthdate"><br>
+    <input type="submit" text="Submit"></form>
+    
+</body>
+</html>
